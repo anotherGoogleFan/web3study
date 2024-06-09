5fa6d8a9f6597213e26492858187901b18ca626c
anotherGoogleFan <16779242@qq.com>
2024-06-09 18:39:52 +0700

    .

---
+++ study-material/classic-solidity-usage/hello_world.sol
@@ -0,0 +1,10 @@
+// Hello World
+
+// pragma specifies the compiler version of Solidity.
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract hello_world {
+    string public greet = "hello world.";
+}
